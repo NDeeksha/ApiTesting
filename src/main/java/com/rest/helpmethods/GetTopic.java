f4ced7540091d8c5e3cfba2a4ec83a3c8c23fa74
NDeeksha <33822929+NDeeksha@users.noreply.github.com>
2017-11-22 14:31:32 +0530

    Added ATUReports description

--- src/main/java/com/rest/helpmethods/GetTopic.java
+++ src/main/java/com/rest/helpmethods/GetTopic.java
@@ -1,5 +1,7 @@
 package com.rest.helpmethods;
 
+import atu.testng.reports.ATUReports;
+
 import com.rest.commonUtils.GetStatus;
 
 public class GetTopic extends SetUp{
@@ -11,19 +13,23 @@ public class GetTopic extends SetUp{
 	 *  getTopicByName
 	 */
 	
+	@SuppressWarnings("deprecation")
 	public void getTopicById(int id) {
 		System.out.println("--------Get Topic By Id: " + id+ "-------");
 		response = httpRequest.get(url+"/topics/"+id);
 	
 		getStatus.getStatusCode(response, "getTopicById");
 		getStatus.displayResponse(response);
+		ATUReports.add("Get Topic By ID", "Get Topic By ID", "Get Topic", true);
 	}
 
+	@SuppressWarnings("deprecation")
 	public void getTopicByName(String name) {
 		System.out.println("--------Get Topic By Name: "+ name+ "-------");
 		response = httpRequest.get(url+"/topics?name="+name);
 	
 		getStatus.getStatusCode(response, "getTopicByName");
 		getStatus.displayResponse(response);
+		ATUReports.add("Get Topic By Name", "Get Topic By Name", "Get Topic", true);
 	}
 }
