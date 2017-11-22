f277442caaeb3bbee1e40652b4a492dc94f93e68
NDeeksha <33822929+NDeeksha@users.noreply.github.com>
2017-11-22 14:36:29 +0530

    Adding groups, ATU reports and description

--- src/test/java/com/rest/testcase/SimpleRestTest.java
+++ src/test/java/com/rest/testcase/SimpleRestTest.java
@@ -3,6 +3,8 @@ package com.rest.testcase;
 import org.json.JSONObject;
 import org.testng.annotations.Test;
 
+import atu.testng.reports.ATUReports;
+
 import com.rest.commonUtils.CreateJsonTemplate;
 import com.rest.commonUtils.GetStatus;
 import com.rest.helpmethods.GetTopic;
@@ -16,7 +18,7 @@ public class SimpleRestTest extends SetUp{
 	int id = 2;
 	String name= "Java";
 	String description = "Quick start Java 2";
-
+	
 	/**
 	 * Test Methods:
 	 * postTopic
@@ -24,8 +26,9 @@ public class SimpleRestTest extends SetUp{
 	 * getTopic
 	 * deleteTopic
 	 */
-	
-	@Test
+
+	@SuppressWarnings("deprecation")
+	@Test(groups = { "bvt","regression" }, description= "Add topics and validate using getTopic" )
 	public void postTopic() {
 		JSONObject topic=jsonTemplate.createTopicJson(id, name, description);
 
@@ -33,11 +36,13 @@ public class SimpleRestTest extends SetUp{
 		
 		response = httpRequest.contentType("application/json").body(topic.toString()).post(url+"/topics");
 		getStatus.getStatusCode(response, "postTopic");
+		ATUReports.add("Verify Add Topic", "Add Topic By ID", "Add Topic", true);
 	
 		topicdetails.getTopicById(id);
 	}
 
-	@Test(dependsOnMethods= { "postTopic"} )
+	@SuppressWarnings("deprecation")
+	@Test(dependsOnMethods= { "postTopic"}, groups = { "regression" }, description= "Update topics and validate using getTopic")
 	public void updateTopic() {
 		JSONObject topic=jsonTemplate.createTopicJson(id, name, "Updated Description");
 		
@@ -45,11 +50,13 @@ public class SimpleRestTest extends SetUp{
 
 		response = httpRequest.contentType("application/json").body(topic.toString()).put(url+"/topics/"+id);
 		getStatus.getStatusCode(response, "updateTopic");
+		ATUReports.add("Verify Update Topic", "Update Topic", "Update Topic", true);
 		
 		topicdetails.getTopicById(id);
 	}
 
-	@Test(dependsOnMethods= { "updateTopic"} )
+	@SuppressWarnings("deprecation")
+	@Test(dependsOnMethods= { "updateTopic"}, groups = { "regression" }, description= "Get All Topics, Validate Get Topic By Name" )
 	public void getTopic() {
 		System.out.println("--------Get All Topics-------");
 		
@@ -58,15 +65,18 @@ public class SimpleRestTest extends SetUp{
 		getStatus.getStatusCode(response,"getTopic");
 		getStatus.displayResponse(response);
 		
+		ATUReports.add("Verify Get Topic", "Get All Topics", "Get All Topics", true);
+		
 		topicdetails.getTopicByName("Java 8");
 	}
 
-	@Test(dependsOnMethods= { "getTopic"} )
+	@SuppressWarnings("deprecation")
+	@Test(dependsOnMethods= { "getTopic"}, groups = { "bvt","regression" }, description= "Delete topics and validate using getTopic" )
 	public void deleteTopic() {
 		System.out.println("--------Delete Topic with id: " + id+ "-------");
 		response = httpRequest.contentType("application/json").delete(url+"/topics/"+id);
 		getStatus.getStatusCode(response, "deleteTopic");
-		
+		ATUReports.add("Verify Delete Topic", "Delete Topic by ID", "Delete Topic By ID", true);
 		//Check if delete is success
 		topicdetails.getTopicById(id);
 	}
